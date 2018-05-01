c4b005baa58419d1919cd21992ee96626595cc28
Pablo Henrique Bertaco <pablo_fonseca91@icloud.com>
2018-05-01 14:20:50 -0300

    Add Scene classes and GameColors

---
+++ CommandersWar/Game/GameColors.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+using Microsoft.Xna.Framework;
+
+namespace CommandersWar.Game
+{
+    public class GameColors
+    {
+        internal static Color backgroundColor = new Color(0.0862745098f, 0.05882352941f, 0.1490196078f);
+
+        internal static Color common = new Color(1.0f, 1.0f, 1.0f);
+        internal static Color uncommon = new Color(0.4815354347f, 1.0f, 0.4763019681f);
+        internal static Color rare = new Color(0.7422516942f, 1.0f, 0.4721496701f);
+        internal static Color heroic = new Color(1.0f, 1.0f, 0.4655374289f);
+        internal static Color epic = new Color(1.0f, 0.7514713407f, 0.4818159938f);
+        internal static Color legendary = new Color(1.0f, 0.4985902309f, 0.4918352962f);
+        internal static Color supreme = new Color(1.0f, 0.4923908114f, 0.7510616779f);
+
+        internal static Color controlBlue = new Color(0f, 0.6078431373f, 0.8862745098f);
+        internal static Color controlRed = new Color(1.0f, 0f, 0.3960784314f);
+        internal static Color controlYellow = new Color(0.9058823529f, 0.662745098f, 0.2470588235f);
+
+        internal static Color blueTeam = new Color(0.5019607843f, 0.5019607843f, 1.0f);
+        internal static Color redTeam = new Color(1.0f, 0.5019607843f, 0.5019607843f);
+        internal static Color explosion = new Color(1.0f, 0.5019607843f, 0.2509803922f);
+
+        internal static Color facebook = new Color(0.231372549f, 0.3490196078f, 0.5960784314f);
+        internal static Color linkedIn = new Color(0f, 0.4666666667f, 0.7098039216f);
+        internal static Color twitter = new Color(0.3333333333f, 0.6745098039f, 0.9333333333f);
+
+        internal static Color points = new Color(0.9058823529f, 0.662745098f, 0.2470588235f);
+        internal static Color premiumPoints = new Color(0f, 0.6078431373f, 0.8862745098f);
+
+        internal static Color fontGreen = new Color(0.5019607843f, 1.0f, 0.5019607843f);
+        internal static Color fontRed = new Color(1.0f, 0.5019607843f, 0.5019607843f);
+
+        internal static Color fire = new Color(1.0f, 0f, 0f);
+        internal static Color ice = new Color(0f, 1.0f, 1.0f);
+        internal static Color wind = new Color(1.0f, 1.0f, 0f);
+        internal static Color earth = new Color(0f, 1.0f, 0f);
+        internal static Color thunder = new Color(1.0f, 0f, 1.0f);
+        internal static Color water = new Color(0f, 0f, 1.0f);
+        internal static Color light = new Color(1.0f, 1.0f, 1.0f);
+        internal static Color darkness = new Color(0.4980392157f, 0.4980392157f, 0.4980392157f);
+    }
+}
